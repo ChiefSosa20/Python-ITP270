db431669c5d58f1dd30888a36bb1da5c61d6d954
ChiefSosa20 <Jos2136@email.vccs.edu>
2023-04-14 01:13:38 +0000

    adding assignment

---
+++ domain_validator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import whois # pip install python-whois
+
+def is_registered(domain_name):
+    """
+    A function that returns a boolean indicating 
+    whether a domain_name is registered
+    """
+    try:
+        w = whois.whois(domain_name)
+    except Exception:
+        return False
+    else:
+        return bool(w.domain_name)
+
+
+if name == "main":
+    print(is_registered("google.com"))
+    print(is_registered("something-that-do-not-exist.com"))
---
+++ hello_world.py
@@ -0,0 +1,7 @@
+
+#!/bin/python3
+
+#Print "Hello World!"
+
+greeting = ("what do you want to input?")
+print (greeting)
